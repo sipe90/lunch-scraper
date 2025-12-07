3a694f23497f07c436e57b877f5205b22acae280
Pasi Härme <pasi.harme@gmail.com>
2025-12-07 21:14:40 +0200

    strips links and images from markdown

--- src/main/kotlin/com/github/sipe90/lunchscraper/scraping/loader/Html2MdConverter.kt
+++ src/main/kotlin/com/github/sipe90/lunchscraper/scraping/loader/Html2MdConverter.kt
@@ -1,13 +1,21 @@
 package com.github.sipe90.lunchscraper.scraping.loader
 
 import com.vladsch.flexmark.html2md.converter.FlexmarkHtmlConverter
+import com.vladsch.flexmark.html2md.converter.LinkConversion
+import com.vladsch.flexmark.util.data.MutableDataSet
 import kotlinx.coroutines.coroutineScope
 
 /**
  * https://github.com/vsch/flexmark-java/wiki/Extensions#html-to-markdown
  */
 object Html2MdConverter {
-    private val converter = FlexmarkHtmlConverter.builder().build()
+    private val settings =
+        MutableDataSet()
+            .set(FlexmarkHtmlConverter.EXT_INLINE_IMAGE, LinkConversion.NONE)
+            .set(FlexmarkHtmlConverter.EXT_INLINE_LINK, LinkConversion.NONE)
+            .toImmutable()
+
+    private val converter = FlexmarkHtmlConverter.builder(settings).build()
 
     suspend fun convertHtmlToMarkdown(html: String): String =
         coroutineScope {
